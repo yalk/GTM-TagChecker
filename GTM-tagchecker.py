405aabbb22bb497b0370d767fbe324e9db24f54a
Yalk <ankur2893@gmail.com>
2015-07-31 18:42:01 +0530

    better formatting for easy excel import

--- GTM-tagchecker.py
+++ GTM-tagchecker.py
@@ -6,16 +6,16 @@ user_agent = 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_4; en-US) AppleWebK
 headers = { 'User-Agent' : user_agent }
 
 #Replace the address string with appropriate values
-address='C:\\Users\\Users\\Desktop\\lodha\\siteLinks'
+address='C:\\Users\\For Users\\Desktop\\lodha\\siteLinks'
 siteLinks = open(address, 'r')
 
 #Used if there is no Google Tag Manager code present at all.
 flag=0
 
 #Replace 295 with number of lines on the file
-for i in range(0,295):
+for i in range(0,442):
 	currentPage=siteLinks.readline()
-	print(""+str(i+1)+",\t"+currentPage),
+	print(""+str(i+1)+", "+str(currentPage.splitlines()[0])+", "),
 
 	#Request the webpage source
 	req = urllib2.Request(currentPage, None, headers)
@@ -25,17 +25,18 @@ for i in range(0,295):
 	flag=0
 
 	#Regular Expression to detect GTM Object IDs. Feel free to change it.
-	RE= re.findall(r'GTM-.*"', page)
+	RE= re.findall(r'dataLayer = \[\]', page)
 	
 	for j in RE:
-		if j.lower()!='GTM-Q8FF"'.lower():
-			print("\t"+j+"\tFAULT\n")
+		#dataLayer = [];
+		if j.lower()!='dataLayer = []'.lower():
+			print(""+j+", FAILED")
 			flag=1
 		else:
-			print("\t"+j+"\tPASSED\n")
+			print(""+j+", PASSED")
 			flag=1
 	if flag==0:
-		print("\tNOCODE\tGTM ABSENT\n")
+		print("NOCODE, ABSENT")
 
 
 #NOTE: If you want to export the output in Excel/Speadsheet, format the print calls and make them appear on the same line. Values should be seperated with a comman (,) Save the output as a CSV file, import that into Excel/Spreadsheet with delimiter as comma (,)
