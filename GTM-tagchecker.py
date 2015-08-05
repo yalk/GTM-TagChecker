9568159cb86f96724365f54703394e496d558c7d
Yalk <yalk@users.noreply.github.com>
2015-08-05 12:34:21 +0530

    etc

--- GTM-tagchecker.py
+++ GTM-tagchecker.py
@@ -5,7 +5,7 @@ import re
 user_agent = 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_4; en-US) AppleWebKit/534.3 (KHTML, like Gecko) Chrome/6.0.472.63 Safari/534.3'
 headers = { 'User-Agent' : user_agent }
 
-#Replace the address string with appropriate values
+#Replace the address string with appropriate values x
 address='C:\\Users\\For Users\\Desktop\\lodha\\siteLinks'
 siteLinks = open(address, 'r')
 
