36ce483bb61a70dcedb6867508a2cd1ffb018659
Yalk <ankur2893@gmail.com>
2015-08-19 15:09:34 +0530

    Better regex and formatting
    This can now do:
    1. capture gtm id properly before an id
    2. better formatting when multiple gtm ids are encountered

--- EverythingChecker.py
+++ EverythingChecker.py
@@ -19,9 +19,9 @@ headerForCSV="No,Link,Retrieved,Status\n"
 outputfile.write(headerForCSV)
 
 #Replace 295 with number of lines on the file
-for i in range(0,60):
+for i in range(0,10):
 	currentPage=siteLinks.readline()
-	outputLine=""+str(i+1)+","+str(currentPage.splitlines()[0])+", "
+	outputLine=""+str(i+1)+","+str(currentPage.splitlines()[0])
 
 	print(outputLine),
 	outputfile.write(outputLine)
@@ -29,7 +29,7 @@ for i in range(0,60):
 	#Request the webpage source
 	req = urllib2.Request(currentPage, None, headers)
 	
-	#UNCOMMENT IF AUTHENTICATION IS REQUIRED ON THE WEBSITE
+	#Uncomment if site needs password
 	#base64string = base64.encodestring('%s:%s' % ('username', 'password')).replace('\n', '')
 	#req.add_header("Authorization", "Basic %s" % base64string)   
 	
@@ -40,16 +40,15 @@ for i in range(0,60):
 	page = re.sub(re.compile("/\*.*?\*/", re.DOTALL) , "", page) 	#Remove all occurance streamed comments (/*Javascript Multiline COMMENT */) from string
 	page = re.sub(re.compile("<!--.*?-->", re.DOTALL) , "", page) 	#Remove all occurance streamed comments (<!--HTML Multiline COMMENT-->) from string
 	#page = re.sub(re.compile("//.*?\n") , "", page) 				#Remove all occurance singleline comments (//COMMENT\n ) from string
-	#NOTE: ^THIS RARELY WORKS. HIGH CHANCE IT'LL BREAK THE HTML CODE BECAUSE // CAN ALSO BE HTTP LINKS.
-	#UPGRADE CAN BE LOOK BEHIND AND LOOK AHEAD REGEXs. LOOK BEHIND FOR http:, LOOK AHEAD FOR wwww./etc.
-
+	
 	flag=0
 
 	#Regular Expression to detect GTM Object IDs. Feel free to change it.
 	#RE= re.findall(r'dataLayer = \[\]', page)
-	RE= re.findall(r'dataLayer ?= ?\[\]', page)
+	#RE= re.findall(r'dataLayer ?= ?\[\]', page)
 	#RE= re.findall(r'gaq\.push', page)
 	#RE= re.findall(r'(?m)^(?!//)Weybeo_widget', page)
+	RE= re.findall(r'GTM-.*?(?=\W)', page) #String starts with 'GTM-.*<followed by anything before a non alphanumeric character>?(?=\W)
 	#RE= re.findall(r'GTM-.*"', page)
 	#RE= re.findall(r'gaq\.push', page)
 
@@ -59,20 +58,20 @@ for i in range(0,60):
 		#dataLayer = [];
 		#if j.lower()=='dataLayer = []'.lower():
 		#if j.lower()=='gaq.push'.lower():
-		if j.lower()=='datalayer = []'.lower():
+		#if j.lower()=='datalayer = []'.lower():
 		#if j.lower()==r'Weybeo_widget'.lower():
-		#if j.lower()==r'GTM-535JCM"'.lower():
-			outputfile.write(""+j+",PASSED\n")
-			print(" "+j+", PASSED"),
+		if j.lower()==r'GTM-IDHERE'.lower():
+			outputfile.write(","+j+",PASSED")
+			print(""+j+", PASSED"),
 			flag=1
 		else:
-			outputfile.write(""+j+",FAILED\n")
-			print(" "+j+", FAILED"),
+			outputfile.write(","+j+",FAILED")
+			print(""+j+", FAILED"),
 			flag=1
-		print ""
 	if flag==0:
-		outputfile.write("NOCODE,ABSENT\n")
+		outputfile.write(",NOCODE,ABSENT")
 		print("NOCODE, ABSENT")
-
+	print ""
+	outputfile.write("\n")
 siteLinks.close()
 outputfile.close()
