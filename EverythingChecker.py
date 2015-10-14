9a19967047505e0b2e22e3e89875713483fe087c
Yalk <ankur2893@gmail.com>
2015-10-14 15:25:32 +0530

    updated

--- EverythingChecker.py
+++ EverythingChecker.py
@@ -6,20 +6,20 @@ user_agent = 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_4; en-US) AppleWebK
 headers = { 'User-Agent' : user_agent }
 
 #Replace the address string with appropriate values
-address='C:\\Users\\For Users\\Desktop\\newTool\\siteLinks'
+address='siteLinks'
 siteLinks = open(address, 'r')
 
 #Used if there is no Google Tag Manager code present at all.
 
 flag=0
-address2='C:\\Users\\For Users\\Desktop\\newTool\\outputCSV.csv'
+address2='outputCSV.csv'
 outputfile = open(address2, 'w')
 
 headerForCSV="No,Link,Retrieved,Status\n"
 outputfile.write(headerForCSV)
 
 #Replace 295 with number of lines on the file
-for i in range(0,10):
+for i in range(0,7352):
 	currentPage=siteLinks.readline()
 	outputLine=""+str(i+1)+","+str(currentPage.splitlines()[0])
 
@@ -30,8 +30,8 @@ for i in range(0,10):
 	req = urllib2.Request(currentPage, None, headers)
 	
 	#Uncomment if site needs password
-	#base64string = base64.encodestring('%s:%s' % ('username', 'password')).replace('\n', '')
-	#req.add_header("Authorization", "Basic %s" % base64string)   
+	base64string = base64.encodestring('%s:%s' % ('admin', 'caresupport')).replace('\n', '')
+	req.add_header("Authorization", "Basic %s" % base64string)   
 	
 	response = urllib2.urlopen(req)
 	page = response.read()
@@ -51,26 +51,29 @@ for i in range(0,10):
 	RE= re.findall(r'GTM-.*?(?=\W)', page) #String starts with 'GTM-.*<followed by anything before a non alphanumeric character>?(?=\W)
 	#RE= re.findall(r'GTM-.*"', page)
 	#RE= re.findall(r'gaq\.push', page)
-
+	#RE= re.findall("ga\('send'*", page)
 	#RE= re.findall(r'GTM.*"', page)
 
 	for j in RE:
 		#dataLayer = [];
 		#if j.lower()=='dataLayer = []'.lower():
-		#if j.lower()=='gaq.push'.lower():
+		#if j.lower()=="ga('send'".lower():
+		#if j.lower()=="gaq.push":
 		#if j.lower()=='datalayer = []'.lower():
 		#if j.lower()==r'Weybeo_widget'.lower():
-		if j.lower()==r'GTM-IDHERE'.lower():
+		if j.lower()==r'GTM-TWGMR3'.lower():
+		#if j.lower()==r'GTM-PHGNZL'.lower():	
+		#if j.lower()==r'GTM-PH'.lower():
 			outputfile.write(","+j+",PASSED")
-			print(""+j+", PASSED"),
+			print(","+j+", PASSED"),
 			flag=1
 		else:
 			outputfile.write(","+j+",FAILED")
-			print(""+j+", FAILED"),
+			print(","+j+", FAILED"),
 			flag=1
 	if flag==0:
 		outputfile.write(",NOCODE,ABSENT")
-		print("NOCODE, ABSENT")
+		print("NOCODE, ABSENT"),
 	print ""
 	outputfile.write("\n")
 siteLinks.close()
