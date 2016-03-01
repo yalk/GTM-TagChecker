b1c5b8ca2b4222c189b0e5d9fc1f4c9d043ea409
Yalk <ankur2893@gmail.com>
2016-03-01 14:28:33 +0530

    v2.9
    New feature:
    - Counts number of codes found
    
    Other Additions:
    - Added functions
    - Better formatting in CSV file

--- GTM-tagchecker.py
+++ GTM-tagchecker.py
@@ -1,109 +1,146 @@
 import urllib2, base64
 import re
+import csv
 
-#Browser related tags
-user_agent = 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_4; en-US) AppleWebKit/534.3 (KHTML, like Gecko) Chrome/6.0.472.63 Safari/534.3'
-headers = { 'User-Agent' : user_agent }
+def fetchpage(URL=None, username=None, password=None):
 
-#Replace the address string with appropriate values
-address='siteLinks'
-siteLinks = open(address, 'r')
+	if(URL!=None):
 
-numberOfLinks=0;
-with open(address) as temp_f:
-    numberOfLinks= sum(1 for _ in temp_f)
-temp_f.close()
-#Used if there is no Google Tag Manager code present at all.
+		user_agent = 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_4; en-US) AppleWebKit/534.3 (KHTML, like Gecko) Chrome/6.0.472.63 Safari/534.3'
+		headers = { 'User-Agent' : user_agent }
 
-flag=0
-address2='outputCSV.csv'
-outputfile = open(address2, 'w')
+		req = urllib2.Request(URL, None, headers)
 
-headerForCSV="No,Link,Retrieved,Status,Retrieved,Status,Retrieved,Status\n"
-outputfile.write(headerForCSV)
-
-#Replace 295 with number of lines on the file
-for i in range(0,numberOfLinks):
-#for i in range(0,10):
-	try:
-		currentPage=siteLinks.readline()
-		outputLine=""+str(i+1)+","+str(currentPage.splitlines()[0])
-
-		print(outputLine),
-		outputfile.write(outputLine)
-
-		#Request the webpage source
-		req = urllib2.Request(currentPage, None, headers)
+		if((username!=None) and (password!=None)):						#for domains with passwords
+			base64string = base64.encodestring('%s:%s' % ('admin', 'caresupport')).replace('\n', '')
+			req.add_header("Authorization", "Basic %s" % base64string)   
+		response = urllib2.urlopen(req)									#fetch page
+		page_source = response.read()
 		
-		#Uncomment if site needs password
-		base64string = base64.encodestring('%s:%s' % ('admin', 'caresupport')).replace('\n', '')
-		req.add_header("Authorization", "Basic %s" % base64string)   
+		#clean up page
+		page_source = re.sub(re.compile("/\*.*?\*/", re.DOTALL) , "", page_source) 	#Remove all occurance streamed comments (/*Javascript Multiline COMMENT */) from string
+		page_source = re.sub(re.compile("<!--.*?-->", re.DOTALL) , "", page_source) 	#Remove all occurance streamed comments (<!--HTML Multiline COMMENT-->) from string
 		
-		response = urllib2.urlopen(req)
-		page = response.read()
-		#Resetting Flag
+		return page_source
+
+
+
+def checkGTM(page_source):
+	
+	GTMStatusList=[None]*3
+	RE_GTM= re.findall(r'GTM-.*?(?=\W)', page_source) #String starts with 'GTM-.*<followed by anything before a non alphanumeric character>?(?=\W)
+	
+	for j in RE_GTM:
+		if (j!=None):
+			if("GTM_YES" not in GTMStatusList):
+				GTMStatusList[0]="GTM_YES"
+			
+			if j not in GTMStatusList:
+				if GTMStatusList[2]==None:
+					GTMStatusList[2]=str(j)
+				
+				else:
+					GTMStatusList[2]=GTMStatusList[2]+"|"+str(j)
 		
-		page = re.sub(re.compile("/\*.*?\*/", re.DOTALL) , "", page) 	#Remove all occurance streamed comments (/*Javascript Multiline COMMENT */) from string
-		page = re.sub(re.compile("<!--.*?-->", re.DOTALL) , "", page) 	#Remove all occurance streamed comments (<!--HTML Multiline COMMENT-->) from string
-		#page = re.sub(re.compile("//.*?\n") , "", page) 				#Remove all occurance singleline comments (//COMMENT\n ) from string
+	if GTMStatusList[0]==None:
+		GTMStatusList[0]="GTM_ABSENT"
+		GTMStatusList[2]="GTM_NO"
+	#print GTMStatusList,
+	return GTMStatusList
+	
+
+
+def checkGA(page_source):
+	
+	GAStatusList=[None]*3
+	RE_GA= re.findall("(ga ?\('create' ?, ?'UA-.*?' ?, ?'auto' ?\))|(gaq\.push\(\['_setAccount' ?, ?'UA-.*?'\]\))",page_source)
+	
+	for k in RE_GA:
+		RE_GA_trimmer= re.findall("UA-.*?-.",str(k))
 		
-		flag=0
-		flag1=0
-		#Regular Expression to detect GTM Object IDs. Feel free to change it.
-		#RE= re.findall(r'dataLayer = \[\]', page)
-		#RE= re.findall(r'dataLayer ?= ?\[\]', page)
-		#RE= re.findall(r'gaq\.push', page)
-		#RE= re.findall(r'(?m)^(?!//)Weybeo_widget', page)
-		RE= re.findall(r'GTM-.*?(?=\W)', page) #String starts with 'GTM-.*<followed by anything before a non alphanumeric character>?(?=\W)
-		#RE= re.findall(r'GTM-.*"', page)
-		#RE= re.findall(r'gaq\.push', page)
-		RE2= re.findall("ga\('send'*", page)
-		#RE= re.findall(r'GTM.*"', page)
+		if(RE_GA_trimmer!=None):
+			if("GA_YES" not in GAStatusList):
+				GAStatusList[0]="GA_YES"
+			
+			if RE_GA_trimmer not in GAStatusList:
+				if GAStatusList[2]==None:
+					GAStatusList[2]=str(RE_GA_trimmer[0])
+				
+				else:
+					GAStatusList[2]=GAStatusList[2]+"|"+str(RE_GA_trimmer[0])
+		
+	if GAStatusList[0]==None:
+		GAStatusList[0]="GA_ABSENT"
+		GAStatusList[2]="GA_NO"
+	#print GAStatusList
+	return GAStatusList
 
-		for j in RE:
-			#dataLayer = [];
-			#if j.lower()=='dataLayer = []'.lower():
-			######if j.lower()=="ga('send'".lower():
-			#if j.lower()=="gaq.push":
-			#if j.lower()=='datalayer = []'.lower():
-			#if j.lower()==r'Weybeo_widget'.lower():
 
-			#GTM
+def init_output(outputCSVFile=None):
+	
+	if(outputCSVFile!=None):
+		headerForCSV="No,URL,GTM_Found,NoOfGTM,GTM_Code,GA_Found,NoOfGA,GA_Code\n"
+		outputCSVFile.write(headerForCSV)	
 
-			if j.lower()==r'GTM-MKVL'.lower():
-				outputfile.write(","+j+",GTM_PASSED")
-				print(","+j+", GTM_PASSED"),
-				flag=1
-			else:
-				outputfile.write(","+j+",GTM_FAILED")
-				print(","+j+", GTM_FAILED"),
-				flag=1
-		if flag==0:
-			outputfile.write(",GTM_NOCODE,GTM_ABSENT")
-			print("GTM_NOCODE, GTM_ABSENT"),
 
-		#GA
 
-		for k in RE2:
-			if k.lower()=="ga('send'".lower():
-				outputfile.write(","+k+",GA_PASSED")
-				print(","+k+", GA_PASSED"),
-				flag1=1
-			else:
-				outputfile.write(","+k+",GA_FAILED")
-				print(","+k+", GA_FAILED"),
-				flag1=1
-		if flag1==0:
-			outputfile.write(",GA_NOCODE,GA_ABSENT")
-			print("GA_NOCODE, GA_ABSENT"),
+def writer_and_printer(outputCSVFile, URL_GTM_and_GA_status):
+	
+	writerobject=csv.writer(outputCSVFile, quoting=csv.QUOTE_ALL)
+	writerobject.writerow(URL_GTM_and_GA_status)
 
-		print ""
-		outputfile.write("\n")
-	except:
-		outputfile.write(",ERROR")
-		print(", ERROR"),
+	print URL_GTM_and_GA_status
 
-		print ""
-		outputfile.write("\n")
-siteLinks.close()
-outputfile.close()
+
+
+def main():
+
+	addressOfInputFile='siteLinks'
+
+	numberOfLinks= 0;
+	
+	with open(addressOfInputFile, 'r') as temp_f:
+	    numberOfLinks= sum(1 for _ in temp_f)
+	temp_f.close()
+
+	addressOfOutputFile= 'outputCSV.csv'
+	outputCSVFile= open(addressOfOutputFile, 'wb')
+	
+	init_output(outputCSVFile)
+
+	siteLinks= open(addressOfInputFile, 'r')
+
+	for i in range(0,numberOfLinks):
+
+		try:
+			URLList= [None]*2
+			URL= siteLinks.readline().splitlines()[0]
+			URLLine= ""+str(i+1)+","+str(URL)
+
+			URLList[0]= i+1
+			URLList[1]= URL
+
+			page_source= fetchpage(URL)
+			
+			GTMStatusList= checkGTM(page_source)
+			GTMs= str(GTMStatusList[2])
+			countOfGTMCodes= int(GTMs.count('|')) +1
+			GTMStatusList[1]= countOfGTMCodes
+			
+
+			GAStatusList= checkGA(page_source)
+			GAs= str(GAStatusList[2])
+			countofGACodes= int(GAs.count('|')) +1
+			GAStatusList[1]= countofGACodes
+
+			URL_GTM_and_GA_status= URLList +GTMStatusList +GAStatusList
+			
+			writer_and_printer(outputCSVFile, URL_GTM_and_GA_status)
+
+		except:
+			continue
+	
+	siteLinks.close()
+	outputCSVFile.close()
+
+if __name__ == "__main__": main()
